69e21bfd7e35cbce60f566732dd75b90be66d307
Maxim <maxonchoo@gmail.com>
2021-08-08 19:10:19 +0300

    task 2 typeOf

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
+let age = 25, //number
+    name = "Maxim", // string
+    bigIn = 123n, // BigInt
+    bool = true, // boolean (true / false)
+    next = null, // Null
+    nex = undefined, // undefined
+    obj = {} ;// object 
 
-let num = 3;
-console.log(num);
-
-
-let a = 10;
-let b = 2;
-console.log(a + b, "," , a - b, "," , a b , ",", a / b);
-
-
-
-let c = 15;
-let d = 2;
-let result = c + d;
-console.log(result); 
-
-
-let a = 10,
-b = 2,
-c = 5;
-console.log(a+b+c); 
-
-
-let a = 17,
-b = 10,
-c = a - b,
-d = 7,
-result = c + d;
-console.log(result); 
+console.log(typeof next);
