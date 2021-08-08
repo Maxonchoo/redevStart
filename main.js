1fc1bc1afc3f45cc0b96f547125a7b087f6db786
Maxim <maxonchoo@gmail.com>
2021-08-08 18:04:39 +0300

    vareable task

--- main.js
+++ main.js
@@ -3,6 +3,41 @@
     console.log(num);
 */
 
+/* task 2
 let a = 10;
 let b = 2;
 console.log(a + b, "," , a - b, "," , a * b , ",", a / b);
+*/
+
+/* task 3
+let c = 15;
+let d = 2;
+let result = c + d;
+console.log(result);
+*/
+
+/*task 4
+let a = 10,
+    b = 2,
+    c = 5;
+console.log(a+b+c);
+*/
+
+/*task 5
+let a = 17,
+    b = 10,
+    c = a - b,
+    d = 7,
+    result = c + d;
+console.log(result);
+*/
+
+
+/* ~~~ task for learn js */
+
+/* task 1 
+let admin ,
+    name = "Джон";
+console.log(admin = name);
+*/
+
