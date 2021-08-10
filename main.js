8e19f54f822bc468a76abf49fa41edc1094046f1
Maxim <maxonchoo@gmail.com>
2021-08-10 13:22:05 +0300

    last commit

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
-//остаток от диления
-//console.log(8%2 == 0);
+/* let NameJs = prompt(`Какое «официальное» название JavaScript?`);
 
-/* возвидение в степень 
-
-console.log(2 ** 1);
-console.log(2 ** 2);
-console.log(2 ** 3);
-console.log(2 ** 4);
-console.log(2 ** 5);
-console.log(2 ** 6);
-console.log(2 ** 7);
-console.log(2 ** 8);
-console.log(2 ** 9);
-console.log(2 ** 10);
-*/
-
-// Условный оператор
-
-/*
-задание 1 
- вариант 1
- let test = true;
-if (test == true) {
-    console.log(`Верно`);
-} else {
-    console.log(`Не верно`);
+if (NameJs == "ECMAScript") {
+    console.log(`Верно!`);
+}else{
+    console.log(`Не Верно!`);
 }
-*/
+(NameJs == "ECMAScript") ? console.log(`Верно!`) : console.log(`Не Верно!`); 
 
-/* задание 1  вариант 2
-test == true ? console.log(`Верно`) : console.log(`Не верно`);
-*/
 
-/* задание 2 вариант 1
-let test = true;
-
-if (test != true) {
-    console.log(`Верно`);
-} else {
-    console.log(`Не верно`);
+let variableJs = +prompt(`Введите число`);
+if (variableJs > 0) {
+    console.log(1);
+} else if(variableJs < 0){
+    console.log(-1);
+}else{
+    console.log(0);
 }
 
-ЗАдание 2 вариант 2 
-test != true ? console.log(`Верно`) : console.log(`Не верно`);
 */
+let variableJs = +prompt(`Введите число`);
+(variableJs > 0) ? console.log(1) : (variableJs < 0) ? console.log(-1) : console.log(0);
