26e7b64ece0e411cba9ed3a42a5170cccc0e69c0
Maxim <maxonchoo@gmail.com>
2021-08-10 14:10:51 +0300

    Logicheskye operatory

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-/* let NameJs = prompt(`Какое «официальное» название JavaScript?`);
+/*let a = -3;
 
-if (NameJs == "ECMAScript") {
-    console.log(`Верно!`);
-}else{
-    console.log(`Не Верно!`);
-}
-(NameJs == "ECMAScript") ? console.log(`Верно!`) : console.log(`Не Верно!`); 
-
-
-let variableJs = +prompt(`Введите число`);
-if (variableJs > 0) {
-    console.log(1);
-} else if(variableJs < 0){
-    console.log(-1);
-}else{
-    console.log(0);
-}
-
+(a > 0 && a < 5) ? console.log(`Верно!`) : console.log(`Не верно`);
 */
-let variableJs = +prompt(`Введите число`);
-(variableJs > 0) ? console.log(1) : (variableJs < 0) ? console.log(-1) : console.log(0);
+
+/*
+let a = 2;
+
+(a == 0 || a == 2) ? console.log(a += 7) : console.log(a /= 10);
+*/
+
+/*
+let a = 3,
+    b = 5;
+
+(a <= 1 && b >= 3) ? console.log(a + b) : console.log(a - b);
+*/
+
+let a = 4,
+    b = 7;
+
+(a > 2 && a < 11 || b >= 6 && b < 14) ? console.log(`Верно`) : console.log(`Неверно`);
