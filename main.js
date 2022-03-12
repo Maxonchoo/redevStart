7157b5066eae0d17775e01ec938bc3b7b9810154
Maxonchoo <maxonchoo@gmail.com>
2022-03-12 10:11:31 +0300

    metod massiva

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-//  const person = {
-//      name : "Максим",
-//      age : 25,
-//      sayHello : 'Hello',
-//      logInfo: function (){
-//          console.log(this.name);
-//      }   
-//  };
-
-//  person.logInfo();
-
-let house = 'slytherin';
-
-switch(house){
-    case 'gryffindor': 
-        console.log('bravery'); 
-        break;
-    case 'hufflepuff': 
-         console.log('loyalty');
-         break;
-    case 'slytherin': 
-         console.log('cunning');
-         break;
-    case 'ravenclaw': 
-         console.log('intellect');
-         break;
-    defoult:
-         console.log('not a valid house');
-         break;
+function camelize(str){
+    let art = [], temp = str.split('-');
+    for(let ti of temp){
+        let compress = ti.slice(0,1).toUpperCase() + ti.slice(1,);
+        art += compress;
+    }
+    
+    console.log(art);
 }
+
+camelize('my-short-string-after-bbd');
